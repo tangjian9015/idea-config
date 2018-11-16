18063d7bf7611a6abeaf837a9096abe79675adc3
tangjian9015 <838545868@qq.com>
2018-11-16 23:36:50 +0800

    idea 配置
    idea 配置

---
+++ idea_config/src/main/java/com/tj/springboot1/Springboot1Application.java
@@ -0,0 +1,22 @@
+package com.tj.springboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class Springboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot1Application.class, args);
+        ArrayList ls;
+    }
+
+    /**
+     *
+     */
+    public void method1() {
+
+    }
+}
